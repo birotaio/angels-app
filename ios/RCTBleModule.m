44c2441c3fc16b477373eaa63583e26289f1adcd
MagikMirouf <slevy.work@gmail.com>
2023-01-20 09:13:39 +0100

    SVC-6917 : Bridge iOS ZoovBLE

--- ios/RCTBleModule.m
+++ ios/RCTBleModule.m
@@ -1,10 +1,10 @@
 #import "RCTBleModule.h"
+#import <React/RCTConvert.h>
 
 @implementation RCTBleModule
 
 // To export a module named RCTBleModule
 RCT_EXPORT_MODULE();
-
 - (NSArray<NSString *> *)supportedEvents {
   return @[@"RCTBleModule"];
 }
@@ -13,8 +13,54 @@ RCT_EXPORT_METHOD(setUp:(nonnull NSString *)url
                   token:(nonnull NSString *)token
                   refreshToken:(nonnull NSString *)refreshToken)
 {
-  NSLog(@"RCTBleModule setUp");
-  [RCTBleModuleSwift setUpWithUrl:url apiKey:nil token:token refreshToken:refreshToken];
+  [RCTBleModuleSwift setUpWithUrl:url token:token refreshToken:refreshToken];
+}
+
+RCT_EXPORT_METHOD(connect:(NSInteger)bikeId
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  [RCTBleModuleSwift connectWithBikeId:bikeId onSuccess:^{
+    resolve(@"12012");
+  } onFailure:^(NSError * _Nonnull error) {
+    reject(@"BLEModule",@"connectWithBikeId",error);
+  }];
+}
+
+RCT_EXPORT_METHOD(disconnect)
+{
+  [RCTBleModuleSwift disconnect];
+}
+
+RCT_EXPORT_METHOD(lockBike:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  [RCTBleModuleSwift lockBikeOnSuccess:^{
+    resolve(@"12012");
+  } onFailure:^(NSError * _Nonnull error) {
+    reject(@"BLEModule",@"lockBike",error);
+  }];
+}
+
+RCT_EXPORT_METHOD(unlockBike:(NSInteger)timeOutToLock
+                  resolver:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  [RCTBleModuleSwift unlockBikeWithTimeOutToLock:timeOutToLock onSuccess:^{
+    resolve(@"12012");
+  } onFailure:^(NSError * _Nonnull error) {
+    reject(@"BLEModule",@"unlockBike",error);
+  }];
+}
+
+RCT_EXPORT_METHOD(unlockBattery:(RCTPromiseResolveBlock)resolve
+                  rejecter:(RCTPromiseRejectBlock)reject)
+{
+  [RCTBleModuleSwift unlockBatteryOnSuccess:^{
+    resolve(@"12012");
+  } onFailure:^(NSError * _Nonnull error) {
+    reject(@"BLEModule",@"unlockBattery",error);
+  }];
 }
 
 
--- ios/RCTBleModuleSwift.swift
+++ ios/RCTBleModuleSwift.swift
@@ -4,13 +4,33 @@
 //
 //  Created by Sylvain Levy on 05/01/2023.
 //
-
 import Foundation
 import ZoovBLEiOS
-
 @objc class RCTBleModuleSwift : NSObject {
  
-  @objc public static func setUp(url: String, apiKey: String?, token: String?, refreshToken: String? = nil) {
-      ZoovBLE.sharedInstance.setUp(url: url, apiKey: apiKey, token: token,refreshToken: refreshToken)
+  private static let TAG = "BleModule"
+  
+  @objc public static func setUp(url: String, token: String?, refreshToken: String? = nil) {
+    ZoovBLE.sharedInstance.setUp(url: url, apiKey: nil, token: token,refreshToken: refreshToken)
   }
+  @objc public static func connect(bikeId: NSInteger,onSuccess: @escaping() -> Void, onFailure: @escaping(Error) -> Void) {
+    ZoovBLE.sharedInstance.connect(bikeId: UInt32(bikeId), onSuccess: onSuccess, onFailure: onFailure)
+  }
+  
+  @objc public static func disconnect() {
+    ZoovBLE.sharedInstance.disconnect()
+  }
+
+  @objc public static func lockBike(onSuccess: @escaping() -> Void, onFailure: @escaping(Error) -> Void) {
+    ZoovBLE.sharedInstance.lock(onSuccess: onSuccess, onFailure: onFailure)
+  }
+
+  @objc public static func unlockBike(timeOutToLock : NSInteger,onSuccess: @escaping() -> Void, onFailure: @escaping(Error) -> Void) {
+    ZoovBLE.sharedInstance.unlock(timeOutToLock: UInt32(timeOutToLock),onSuccess: onSuccess, onFailure: onFailure)
+  }
+
+  @objc public static func unlockBattery(onSuccess: @escaping() -> Void, onFailure: @escaping(Error) -> Void) {
+    ZoovBLE.sharedInstance.unlockBattery(onSuccess: onSuccess, onFailure: onFailure)
+  }
+
 }
